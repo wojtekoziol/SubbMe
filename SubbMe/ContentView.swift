ba2e41b8502f36310919adfbd5f231b86f38bc32
Wojciech Kozioł <wojtek.koziol.pl@gmail.com>
2024-11-07 14:30:47 +0100

    Initial commit

---
+++ SubbMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubbMe
+//
+//  Created by Wojciech Kozioł on 07/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubbMe/SubbMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubbMeApp.swift
+//  SubbMe
+//
+//  Created by Wojciech Kozioł on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SubbMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
